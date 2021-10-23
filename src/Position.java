94097a687fbab9a76ef2bc06d75e9f81f0b0c93e
GomathiKanagaraj <gomathi2641999@gmail.com>
2021-10-23 17:46:11 +0530

    Project change

--- src/Position.java
+++ src/Position.java
@@ -1,7 +1,7 @@
 
 public class Position {
 	public static void main(String[] args) {
-		String s1 = "Hello world";
+		String s1 = "Hello";
 		int index1 = s1.charAt(0);
 		int  intex2 = s1.charAt(3);
 		System.out.println((char)index1);
