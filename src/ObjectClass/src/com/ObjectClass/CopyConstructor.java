8f5bb4d73b17776ea1b6863bb45b44f27f2fc4ed
GomathiKanagaraj <gomathi2641999@gmail.com>
2021-10-26 10:58:17 +0530

    Second commit

---
+++ src/ObjectClass/src/com/ObjectClass/CopyConstructor.java
@@ -0,0 +1,28 @@
+package com.ObjectClass;
+
+public class CopyConstructor {
+	String n;
+	int num;
+	CopyConstructor(String name, int id){
+		n=name;
+		num=id;
+		
+	}
+	CopyConstructor(CopyConstructor C){
+		n = C.n;
+		num =C.num;
+		
+	}
+	void display() {
+		System.out.println(n+" "+num);
+		
+	}
+public static void main(String[] args) {
+	
+	CopyConstructor Con = new CopyConstructor("abi",123);
+	CopyConstructor Con1 = new CopyConstructor(Con);
+	Con.display();
+	Con1.display();
+	
+}
+}
---
+++ src/ObjectClass/src/com/ObjectClass/Method.java
@@ -0,0 +1,11 @@
+package com.ObjectClass;
+
+public class Method {
+	static void  MyMethod () {
+		System.out.println("hello");
+	}
+	public static void main(String[] args) {
+	MyMethod();
+	}
+
+}
